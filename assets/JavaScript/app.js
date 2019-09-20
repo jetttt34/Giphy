765d21b1206081c365080cea8ba43666765c0548
Jed garza <Jedgarza@gmail.com>
2019-09-20 09:46:19 -0500

    fixed code but now api key is returning 403 error

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,27 +1,10 @@
-var buttonChoices = ["Sasuke", "Full Metal Alchemist", "Dragon Ball Z", "SAO", "One Punch Man", "Attack on Tittan", "The Seven Deadly Sins", "Fairy Tail", "Shokugeki no Soma", "Bleach", "My Hero Academia" ];
-
-function createButtons()
-    {
-    document.getElementById("buttonsDiv").innerHTML =""; 
-        for(var i=0;i<buttonChoices.length;i++){
-        varNewButton = "<button class='buttonToClick' onclick = 'getGiphyData(\""+buttonChoices[i]+"\")' >" +buttonChoices[i]+ "</button>";
-        document.getElementById("buttonsDiv").innerHTML += varNewButton;
-        }
-    }
-
-
-document.getElementById("addTopic").addEventListener("click", function () {
-    buttonChoices.push( document.getElementById("topic").value);
-    createButtons();
-});
-
 function getGiphyData(topic) {
-    document.getElementById("results").innerHTML = "Gif's coming Soon!";
-
+    document.getElementById("results").innerHTML = "Gifs are out of Service";
+    
     var response = [];
     response.length = 0;
     $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=cb4rAUUugv4ls0srWM88sPsfKiYgFIyg&q=" + topic + "&offset=0&rating=PG-13&lang=en&limit=20",
+        url: "http://api.giphy.com/v1/gifs/search?q&api_key=AZrV8LOvNX2HOG2Dpde1nedKHyitDBIq"+topic+"&offset=0&rating=PG-13&lang=en&limit=20",
         method: "GET"
     }).then(function (response) {
         console.log(response);
@@ -35,6 +18,20 @@ function getGiphyData(topic) {
     });
 }
 
+var buttonChoices = ["Sasuke", "Full Metal Alchemist", "Dragon Ball Z", "SAO", "One Punch Man", "Attack on Tittan", "The Seven Deadly Sins", "Fairy Tail", "Shokugeki no Soma", "Bleach", "My Hero Academia" ];
+
+function createButtons()
+    {
+    document.getElementById("buttonsDiv").innerHTML =""; 
+        for(var i=0;i<buttonChoices.length;i++){
+        varNewButton = "<button class='buttonToClick' onclick = 'getGiphyData(\""+buttonChoices[i]+"\")' >" +buttonChoices[i]+ "</button>";
+        document.getElementById("buttonsDiv").innerHTML += varNewButton;
+        }
+    }
+document.getElementById("addTopic").addEventListener("click", function () {
+    buttonChoices.push( document.getElementById("topic").value);
+    createButtons();
+});
 
 
 createButtons();
